2475b69f348760b5f9028846c9ba07dc241cf36e
Brendan Jackman <bhenryj0117@gmail.com>
2015-02-27 14:08:06 +0000

    yeah

--- js/main.js
+++ js/main.js
@@ -1,1 +1,43 @@
+window.onload = function() {
+    audio = document.getElementById("audio");
+    canvas = document.getElementById("canvas");
 
+    var drawCtx = canvas.getContext("2d");
+
+    var audioCtx = new AudioContext();
+    var audioSrc = audioCtx.createMediaElementSource(audio);
+    var analyser = audioCtx.createAnalyser();
+    audioSrc.connect(analyser);
+    audioSrc.connect(audioCtx.destination);
+
+    try {
+      analyser.fftSize = canvas.height * 8;
+      if (analyser.frequencyBinCount != canvas.height * 4) {
+          console.log("wat");
+      }
+    } catch (e) {
+      console.log("CANVAS HEIGHT MUST BE A POWER OF 2");
+    }
+
+    var freqData = new Uint8Array(analyser.frequencyBinCount);
+
+    var columnPos = 0;
+    var playing = false;
+    function renderFrame() {
+        if (playing) requestAnimationFrame(renderFrame);
+        // update data in frequencyData
+        analyser.getByteFrequencyData(freqData);
+        var column = new Uint8ClampedArray(freqData, 0, freqData.length);
+        var imageData = new ImageData(column, 1);
+        drawCtx.putImageData(imageData, columnPos, 0);
+        columnPos = (columnPos + 1) % canvas.width;
+    }
+
+    audio.addEventListener("play", function() {
+        playing = true;
+        renderFrame()
+    });
+    audio.addEventListener("pause", function() {
+        playing = false
+    });
+}
